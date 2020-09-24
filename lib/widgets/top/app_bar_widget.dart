1f2bb9b40cbc74c769b42c65ccf0103273c1bdba
Maozk <549595297@qq.com>
2020-09-24 10:04:03 +0800

    通用 AppBar

--- lib/widgets/top/app_bar_widget.dart
+++ lib/widgets/top/app_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:fitee/theme/app_theme.dart';
+import 'package:fitee/utils/nav_util.dart';
 import 'package:fitee/utils/screen.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +9,49 @@ class AppBarWidget extends StatelessWidget{
   final Function callBack;
   final Icon icon;
   final Color color;
+  final bool back;
 
-  AppBarWidget({Key key, this.title, this.callBack, this.icon, this.color}): super(key: key);
+  AppBarWidget({Key key, this.title, this.callBack, this.icon, this.color, this.back = false}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    print(this.back);
+    return back == true ? Container(
+      color: Colors.white,
+      child: AppBar(
+        title: Text(title,
+          style: TextStyle(
+              fontSize: duSetFontSize(18),
+              color: AppTheme.darkText,
+              fontWeight: FontWeight.w700
+          ),
+        ),
+        leading: InkWell(
+          borderRadius:
+          BorderRadius.circular(AppBar().preferredSize.height),
+          child: Icon(Icons.arrow_back, color: Colors.black),
+          onTap: ()=>{
+            NavUtil.pop()
+          },
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: <Widget>[
+          InkWell(
+            borderRadius:
+            BorderRadius.circular(AppBar().preferredSize.height),
+            child: icon == null ? const SizedBox() : Icon(
+              icon.icon,
+              color: AppTheme.dark_grey,
+            ),
+            onTap: () {
+              callBack();
+            },
+          ),
+        ],
+      ),
+    ) :
+    Container(
       color: color == null ? Colors.white : color,
       height: AppBar().preferredSize.height,
       child: Row(
@@ -24,7 +62,7 @@ class AppBarWidget extends StatelessWidget{
             child: Container(
               width: AppBar().preferredSize.height - 8,
               height: AppBar().preferredSize.height - 8,
-            ),
+            )
           ),
           Expanded(
             child: Center(
