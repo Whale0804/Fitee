d2ccf9fe2b49b09a54e434b006970a005600c1d8
Maozk <549595297@qq.com>
2020-10-23 09:15:57 +0800

    更新 README.md

--- lib/main.dart
+++ lib/main.dart
@@ -49,10 +49,10 @@ class MyApp extends StatelessWidget {
     final _darkMode = Provider.of<ThemeModel>(context).darkMode;
 
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      // //全局设置透明
-      // statusBarColor: Colors.transparent,
-      // statusBarIconBrightness: Brightness.dark,
-      // statusBarBrightness: Platform.isAndroid ? Brightness.dark : Brightness.light,
+      //全局设置透明
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Platform.isAndroid ? Brightness.dark : Brightness.light,
       //虚拟按键颜色
       systemNavigationBarColor: Colors.white,
       systemNavigationBarDividerColor: Colors.black,
