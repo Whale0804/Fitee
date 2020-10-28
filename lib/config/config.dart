cb45de6a15304a4383c329e9749488b284185b3a
Maozk <549595297@qq.com>
2020-10-28 13:37:12 +0800

    登录动画

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,8 +1,8 @@
 
 const SERVER_API_URL = 'https://gitee.com';
 const GITHUB_SERVER_API_URL = 'https://api.github.com';
-const CLIENT_ID = '940dbcb165445181d3bbde6a1715176d92737b16dd71e8b7a7bf4e80597960d5';
-const CLIENT_SECRET = 'cc5f4639b918ad2df25a8f8e3578e52312522a7f61462029f3568e60e7410e4c';
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
 
 class AppConfig {
   AppConfig._();
