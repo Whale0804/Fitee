81a73d63cf762d6b86052e7f5e2b1a1ee3ab719d
Maozk <549595297@qq.com>
2020-10-29 22:44:21 +0800

    fix bug

--- lib/model/repository/repository_provider.dart
+++ lib/model/repository/repository_provider.dart
@@ -68,7 +68,11 @@ class ReposProvider with ChangeNotifier{
   }
 
   fetchLastRelease({String fullName}) async {
-    release = await ReleaseApi.fetchLastRelease(fullName: fullName);
+    try {
+      release = await ReleaseApi.fetchLastRelease(fullName: fullName);
+    }catch (e) {
+      release = null;
+    }
     return release;
   }
 }
